564e1e78b289ea7f41fb1d5fbc90e9b9e12aeae0
Simone Gaboli <s.gaboli@gsoftconsulting.it>
2022-11-16 20:14:28 +0100

    Insert test controls

--- BlazorMovies/Client/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
+++ BlazorMovies/Client/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
@@ -13,84 +13,84 @@ namespace BlazorMovies.Client.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\File Simone\Sviluppo\3_corsi\10_Blazor_Course\BlazorMovies\BlazorMovies\Client\_Imports.razor"
+#line 1 "C:\File Simone\Sviluppo\3_corsi\10_Blazor_Course\Movie\BlazorMovies\Client\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\File Simone\Sviluppo\3_corsi\10_Blazor_Course\BlazorMovies\BlazorMovies\Client\_Imports.razor"
+#line 2 "C:\File Simone\Sviluppo\3_corsi\10_Blazor_Course\Movie\BlazorMovies\Client\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\File Simone\Sviluppo\3_corsi\10_Blazor_Course\BlazorMovies\BlazorMovies\Client\_Imports.razor"
+#line 3 "C:\File Simone\Sviluppo\3_corsi\10_Blazor_Course\Movie\BlazorMovies\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\File Simone\Sviluppo\3_corsi\10_Blazor_Course\BlazorMovies\BlazorMovies\Client\_Imports.razor"
+#line 4 "C:\File Simone\Sviluppo\3_corsi\10_Blazor_Course\Movie\BlazorMovies\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\File Simone\Sviluppo\3_corsi\10_Blazor_Course\BlazorMovies\BlazorMovies\Client\_Imports.razor"
+#line 5 "C:\File Simone\Sviluppo\3_corsi\10_Blazor_Course\Movie\BlazorMovies\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\File Simone\Sviluppo\3_corsi\10_Blazor_Course\BlazorMovies\BlazorMovies\Client\_Imports.razor"
+#line 6 "C:\File Simone\Sviluppo\3_corsi\10_Blazor_Course\Movie\BlazorMovies\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\File Simone\Sviluppo\3_corsi\10_Blazor_Course\BlazorMovies\BlazorMovies\Client\_Imports.razor"
+#line 7 "C:\File Simone\Sviluppo\3_corsi\10_Blazor_Course\Movie\BlazorMovies\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\File Simone\Sviluppo\3_corsi\10_Blazor_Course\BlazorMovies\BlazorMovies\Client\_Imports.razor"
+#line 8 "C:\File Simone\Sviluppo\3_corsi\10_Blazor_Course\Movie\BlazorMovies\Client\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\File Simone\Sviluppo\3_corsi\10_Blazor_Course\BlazorMovies\BlazorMovies\Client\_Imports.razor"
+#line 9 "C:\File Simone\Sviluppo\3_corsi\10_Blazor_Course\Movie\BlazorMovies\Client\_Imports.razor"
 using BlazorMovies.Client;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\File Simone\Sviluppo\3_corsi\10_Blazor_Course\BlazorMovies\BlazorMovies\Client\_Imports.razor"
+#line 10 "C:\File Simone\Sviluppo\3_corsi\10_Blazor_Course\Movie\BlazorMovies\Client\_Imports.razor"
 using BlazorMovies.Client.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\File Simone\Sviluppo\3_corsi\10_Blazor_Course\BlazorMovies\BlazorMovies\Client\_Imports.razor"
+#line 11 "C:\File Simone\Sviluppo\3_corsi\10_Blazor_Course\Movie\BlazorMovies\Client\_Imports.razor"
 using BlazorMovies.Client.Helpers;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "C:\File Simone\Sviluppo\3_corsi\10_Blazor_Course\BlazorMovies\BlazorMovies\Client\_Imports.razor"
+#line 12 "C:\File Simone\Sviluppo\3_corsi\10_Blazor_Course\Movie\BlazorMovies\Client\_Imports.razor"
 using BlazorMovies.Shared.Entities;
 
 #line default
